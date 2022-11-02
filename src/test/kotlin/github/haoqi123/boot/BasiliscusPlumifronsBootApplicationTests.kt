bade3ad8c5bffee995293b58357d5ab0adc1e78a
_haoqi <1148648445@qq.com>
2022-11-02 11:06:25 +0800

    删除无用代码

--- src/test/kotlin/github/haoqi123/boot/BasiliscusPlumifronsBootApplicationTests.kt
+++ src/test/kotlin/github/haoqi123/boot/BasiliscusPlumifronsBootApplicationTests.kt
@@ -3,9 +3,18 @@ package github.haoqi123.boot
 import github.haoqi123.boot.controller.TableController
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.context.TypeExcludeFilter
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 
 @SpringBootTest
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan
 class BasiliscusPlumifronsBootApplicationTests {
 
     @Autowired
