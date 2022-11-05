1c0a24cfc8b388693f1faf1aebb65a15059a5c66
haoqi <1148648445@qq.com>
2022-11-05 13:18:36 +0800

    add some code

--- src/main/kotlin/github/haoqi123/boot/helps/WrapperUtils.kt
+++ src/main/kotlin/github/haoqi123/boot/helps/WrapperUtils.kt
@@ -3,45 +3,29 @@ package github.haoqi123.boot.helps
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
 import github.haoqi123.boot.BeanPropertyUtils
 import github.haoqi123.boot.annos.SelectionKeysEnum
+import github.haoqi123.boot.base.dto.FieldAndAnno
 import org.springframework.beans.BeanWrapper
 import org.springframework.beans.BeanWrapperImpl
 
 object WrapperUtils {
 
     fun <E, V : Any, D : Any> generateWrapper(v: V, d: D): QueryWrapper<E> {
-        val wrapper: QueryWrapper<E> = QueryWrapper<E>()
-        val bean: BeanWrapper = BeanWrapperImpl(v)
-        BeanPropertyUtils.getNotNonPropertyNames(bean).apply {
-            BeanPropertyUtils.getPropertyValue(bean, this)
-        }.forEach {
-            when (it.value.selectionKeysEnum) {
-                SelectionKeysEnum.EQ -> {
-                    wrapper.eq(it.key, it.value.fieldValue)
-                }
-
-                SelectionKeysEnum.LIKE -> {
-                    wrapper.like(it.key, it.value.fieldValue)
-                }
-
-                SelectionKeysEnum.RLIKE -> {
-                    wrapper.like(it.key, "%" + it.value.fieldValue + "%")
-                }
-
-                SelectionKeysEnum.IN -> {
-                    wrapper.`in`(it.key, it.value.fieldValue)
-                }
-            }
-        }
+        val wrapper: QueryWrapper<E> = generateWrapper(v)
         wrapper.select(BeanPropertyUtils.getPropertyNames(BeanWrapperImpl(d)))
         return wrapper
     }
 
     fun <E, V : Any> generateWrapper(v: V): QueryWrapper<E> {
-        val wrapper: QueryWrapper<E> = QueryWrapper<E>()
         val bean: BeanWrapper = BeanWrapperImpl(v)
-        BeanPropertyUtils.getNotNonPropertyNames(bean).apply {
+        val map: Map<String, FieldAndAnno> = BeanPropertyUtils.getNotNonPropertyNames(bean).apply {
             BeanPropertyUtils.getPropertyValue(bean, this)
-        }.forEach {
+        }
+        return wrapper(map)
+    }
+
+    private fun <E> wrapper(map: Map<String, FieldAndAnno>): QueryWrapper<E> {
+        val wrapper: QueryWrapper<E> = QueryWrapper<E>()
+        map.forEach {
             when (it.value.selectionKeysEnum) {
                 SelectionKeysEnum.EQ -> {
                     wrapper.eq(it.key, it.value.fieldValue)
