51190a9f67d3193bcecb7f7d327e0b73d6ff2c97
haoqi <1148648445@qq.com>
2022-12-11 14:13:49 +0800

    调整selectionKeys中可用范围

--- src/main/kotlin/github/haoqi123/boot/annos/SelectionKeys.kt
+++ src/main/kotlin/github/haoqi123/boot/annos/SelectionKeys.kt
@@ -1,6 +1,12 @@
 package github.haoqi123.boot.annos
 
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER)
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.FIELD,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.PROPERTY_GETTER
+)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 annotation class SelectionKeys(val value: SelectionKeysEnum)
